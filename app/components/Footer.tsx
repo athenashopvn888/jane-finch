8f148027c9dc37ba96d26f340825c66589b656da
Alexander <sixttrading@gmail.com>
2026-06-19 12:04:26 +0700

    Add GBP local SEO landing pages with manual overrides

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -74,6 +74,7 @@ export default function Footer() {
               <Link href="/info/cheap-weed-york">Cheap Weed North York</Link>
               <Link href="/info/native-cigarettes-york">Native Cigarettes</Link>
               <Link href="/info/weed-store-near-mississauga">Weed Store Near Mississauga</Link>
+              <Link href="/weed-dispensary-north-york/">Jane Finch Cannabis Weed Dispensary in North York</Link>
               <Link href="/contact">Contact Us</Link>
               <a
                 href="https://maps.google.com/?q=2728+Jane+St,+North+York,+ON+M3L+2G6"
